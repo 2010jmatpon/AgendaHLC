b9dd1ec43d1890431f275c5f945a204c87bffaaf
2010jmatpon <jmatpon2010@g.educaand.es>
2023-10-18 09:56:32 +0200

    Commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,13 +6,17 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    HttpClientModule
   ],
   declarations: [HomePage]
 })
